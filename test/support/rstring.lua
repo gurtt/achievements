191452e39e5292661e3cd3ef5efff62c5ac33104
gurtt <6712553+gurtt@users.noreply.github.com>
2023-09-07 20:11:25 +1000

    Just kidding: for sure this is good now

--- test/support/rstring.lua
+++ test/support/rstring.lua
@@ -4,14 +4,14 @@ local sets = { { 97, 122 }, { 65, 90 }, { 48, 57 } } -- a-z, A-Z, 0-9
 ---@param length? number the length of the string. Defaults to 10.
 ---@return string # the random string.
 local function rstring(length)
-	if type and type(length) ~= "number" then
+	if length ~= nil and type(length) ~= "number" then
 		error("length must be a number", 2)
 	end
 	local len = length or 10
 
 	local str = ""
+	math.randomseed(math.floor(os.time() * os.clock()))
 	for i = 1, len do
-		math.randomseed(os.clock() ^ 5)
 		local charset = sets[math.random(1, #sets)]
 		str = str .. string.char(math.random(charset[1], charset[2]))
 	end
