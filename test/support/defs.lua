01c300620bad509ba260337b6470a549a8723af6
gurtt <6712553+gurtt@users.noreply.github.com>
2023-09-07 23:08:48 +1000

    Deep-copy achievement defs when generating

--- test/support/defs.lua
+++ test/support/defs.lua
@@ -1,14 +1,20 @@
+local deepcopy = require("test.support.deepcopy")
 local defs = {}
 
 ---Generates a valid achievement definition collection for the current schema version.
 -- This doesn't verify the passed achievement definitions.
+-- Achievement definitions are deep-copied.
 ---@param defTable table An array of achievement definitions.
 ---@return table The generated definition collection.
 function defs.generate(defTable)
 	local defCollection = {}
 
 	defCollection["$schema"] = "https://raw.githubusercontent.com/gurtt/achievements/v2.0.0/achievements.schema.json"
-	defCollection["achievements"] = defTable
+
+	defCollection.achievements = {}
+	for _, def in pairs(defTable) do
+		defCollection.achievements[def.id] = deepcopy(def)
+	end
 
 	return defCollection
 end
