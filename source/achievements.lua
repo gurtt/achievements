2e01f96715410aba6d3d3440c1feb404878cec88
gurtt <6712553+gurtt@users.noreply.github.com>
2023-09-10 04:46:06 +1000

    Refactor storage operations to separate module

--- source/achievements.lua
+++ source/achievements.lua
@@ -1,16 +1,4 @@
-local deepCopy = require("deepCopy")
-local migrate = require("migration")
----Path where the achievements data for the game is saved.
----@type string
-local PRIVATE_ACHIEVEMENTS_PATH = "achievements.json"
-
----Current achievements data schema version.
----@type number
-local CURRENT_SCHEMA_VERSION = 3
-
----URL for the current schema definition.
----@type string
-local SCHEMA_URL = "https://raw.githubusercontent.com/gurtt/achievements/v3.0.0/achievements.schema.json"
+local storage = require("storage")
 
 ---@diagnostic disable-next-line: lowercase-global
 achievements = {}
@@ -82,86 +70,6 @@ function achievements.set(achievementID, value)
 	end
 end
 
----Loads saved achievement data for the game.
----@param minimumSchemaVersion? number The earliest version of the achievements data schema to try migrating from. If unspecified, migration is disabled.
-local function load(minimumSchemaVersion)
-	if minimumSchemaVersion and type(minimumSchemaVersion) ~= "number" then
-		error("bad argument #1 to 'load' (expected number, got " .. type(minimumSchemaVersion) .. ")", 2)
-	end
-
-	-- Check if saved data exists
-	local savedData = json.decodeFile(PRIVATE_ACHIEVEMENTS_PATH)
-
-	if not savedData then
-		return
-	end
-
-	migrate(savedData, minimumSchemaVersion)
-
-	-- Copy saved data to achievements
-	local sAch = {}
-	for _, ach in ipairs(savedData.achievements) do
-		for _, key in ipairs({ "id", "name", "lockedDescription", "unlockedDescription" }) do
-			if type(ach[key]) ~= "string" then
-				error("invalid type for field '" .. key .. "' (expected string, got " .. type(ach[key]) .. ")")
-			end
-			if ach[key] == "" then
-				error("invalid value for field '" .. key .. "' (expected non-empty string)")
-			end
-		end
-
-		if sAch[ach.id] then
-			error("invalid value for field 'ID' (expected unique value)")
-		end
-
-		if type(ach.maxValue) ~= "nil" then -- saved ach is numeric
-			if type(ach.maxValue) ~= "number" then
-				error("invalid type for field 'maxValue' (expected number, got " .. type(ach.maxValue) .. ")")
-			end
-
-			if ach.maxValue % 1 ~= 0 or ach.maxValue < 1 then
-				error("invalid value for field 'maxValue' (expected positive integer, got " .. ach.maxValue .. ")")
-			end
-
-			if type(ach.value) ~= "number" then
-				error("invalid value for field 'value' (expected number, got " .. type(ach.value) .. ")")
-			end
-
-			if ach.value >= ach.maxValue then
-				if type(ach.unlockedAt) ~= "number" then
-					error("invalid type for field 'unlockedAt' (expected number, got " .. type(ach.unlockedAt) .. ")")
-				end
-
-				if ach.unlockedAt % 1 ~= 0 or ach.unlockedAt < 0 then
-					error(
-						"invalid value for field 'unlockedAt' (expected positive integer, got " .. ach.unlockedAt .. ")"
-					)
-				end
-			end
-		else -- saved ach is boolean
-			if type(ach.value) ~= "boolean" then
-				error("invalid type for field 'value' (expected boolean, got " .. type(ach.value) .. ")")
-			end
-
-			if ach.value == true then
-				if type(ach.unlockedAt) ~= "number" then
-					error("invalid type for field 'unlockedAt' (expected number, got " .. type(ach.unlockedAt) .. ")")
-				end
-
-				if ach.unlockedAt % 1 ~= 0 or ach.unlockedAt < 0 then
-					error(
-						"invalid value for field 'unlockedAt' (expected positive integer, got " .. ach.unlockedAt .. ")"
-					)
-				end
-			end
-		end
-
-		sAch[ach.id] = deepCopy(ach)
-	end
-
-	return sAch
-end
-
 ---Set up the achievements system.
 -- Loads any existing achievements data from the game directory.
 -- The achievement definitions you pass are authoritative; if saved data exists for an achievement you don't define here, that data will be removed next time you call `save()`.
@@ -179,7 +87,7 @@ function achievements.init(achievementDefs, minimumSchemaVersion)
 	end
 
 	-- Load achievements from saved data
-	local _, sAch = xpcall(load, function(msg)
+	local _, sAch = xpcall(storage.load, function(msg)
 		warn("Error loading saved achievement data: " .. msg)
 	end, minimumSchemaVersion)
 
@@ -204,7 +112,7 @@ function achievements.init(achievementDefs, minimumSchemaVersion)
 			end
 
 			if achDef.maxValue % 1 ~= 0 or achDef.maxValue < 1 then
-				error("invalid value for field 'maxValue' (expected positive integer, got " .. achdef.maxValue .. ")")
+				error("invalid value for field 'maxValue' (expected positive integer, got " .. achDef.maxValue .. ")")
 			end
 		end
 
@@ -257,20 +165,9 @@ function achievements.init(achievementDefs, minimumSchemaVersion)
 	end
 end
 
----Persist the current achievements data to storage.
--- It's a good idea to call this during game lifecycle events like `playdate.gameWillTerminate()`, `playdate.deviceWillSleep()`, and `playdate.deviceWillLock()`.
 function achievements.save()
-	local savedData = {}
-
-	savedData["$schema"] = SCHEMA_URL
-	savedData.achievements = {}
-	for _, ach in pairs(achievements.kAchievements) do
-		table.insert(savedData.achievements, ach)
-	end
-
-	json.encodeToFile(PRIVATE_ACHIEVEMENTS_PATH, savedData)
+	storage.save(achievements.kAchievements)
 end
-
 ---Grant the specified achievement if it is a boolean achievement.
 -- This only works for boolean achievements. For numeric achievements, use `set` or `increment`.
 ---@param achievementID string The ID of the achievement to unlock.
---
+++ source/storage.lua
@@ -0,0 +1,102 @@
+local deepCopy = require("deepCopy")
+local migrate = require("migration")
+
+local PRIVATE_ACHIEVEMENTS_PATH = "achievements.json"
+local SCHEMA_URL = "https://raw.githubusercontent.com/gurtt/achievements/v3.0.0/achievements.schema.json"
+
+local storage = {}
+---Loads saved achievement data for the game.
+---@param minimumSchemaVersion? number The earliest version of the achievements data schema to try migrating from. If unspecified, migration is disabled.
+function storage.load(minimumSchemaVersion)
+	if minimumSchemaVersion and type(minimumSchemaVersion) ~= "number" then
+		error("bad argument #1 to 'load' (expected number, got " .. type(minimumSchemaVersion) .. ")", 2)
+	end
+
+	-- Check if saved data exists
+	local savedData = json.decodeFile(PRIVATE_ACHIEVEMENTS_PATH)
+
+	if not savedData then
+		return
+	end
+
+	migrate(savedData, minimumSchemaVersion)
+
+	-- Copy saved data to achievements
+	local sAch = {}
+	for _, ach in ipairs(savedData.achievements) do
+		for _, key in ipairs({ "id", "name", "lockedDescription", "unlockedDescription" }) do
+			if type(ach[key]) ~= "string" then
+				error("invalid type for field '" .. key .. "' (expected string, got " .. type(ach[key]) .. ")")
+			end
+			if ach[key] == "" then
+				error("invalid value for field '" .. key .. "' (expected non-empty string)")
+			end
+		end
+
+		if sAch[ach.id] then
+			error("invalid value for field 'ID' (expected unique value)")
+		end
+
+		if type(ach.maxValue) ~= "nil" then -- saved ach is numeric
+			if type(ach.maxValue) ~= "number" then
+				error("invalid type for field 'maxValue' (expected number, got " .. type(ach.maxValue) .. ")")
+			end
+
+			if ach.maxValue % 1 ~= 0 or ach.maxValue < 1 then
+				error("invalid value for field 'maxValue' (expected positive integer, got " .. ach.maxValue .. ")")
+			end
+
+			if type(ach.value) ~= "number" then
+				error("invalid value for field 'value' (expected number, got " .. type(ach.value) .. ")")
+			end
+
+			if ach.value >= ach.maxValue then
+				if type(ach.unlockedAt) ~= "number" then
+					error("invalid type for field 'unlockedAt' (expected number, got " .. type(ach.unlockedAt) .. ")")
+				end
+
+				if ach.unlockedAt % 1 ~= 0 or ach.unlockedAt < 0 then
+					error(
+						"invalid value for field 'unlockedAt' (expected positive integer, got " .. ach.unlockedAt .. ")"
+					)
+				end
+			end
+		else -- saved ach is boolean
+			if type(ach.value) ~= "boolean" then
+				error("invalid type for field 'value' (expected boolean, got " .. type(ach.value) .. ")")
+			end
+
+			if ach.value == true then
+				if type(ach.unlockedAt) ~= "number" then
+					error("invalid type for field 'unlockedAt' (expected number, got " .. type(ach.unlockedAt) .. ")")
+				end
+
+				if ach.unlockedAt % 1 ~= 0 or ach.unlockedAt < 0 then
+					error(
+						"invalid value for field 'unlockedAt' (expected positive integer, got " .. ach.unlockedAt .. ")"
+					)
+				end
+			end
+		end
+
+		sAch[ach.id] = deepCopy(ach)
+	end
+
+	return sAch
+end
+
+---Persist the current achievements data to storage.
+-- It's a good idea to call this during game lifecycle events like `playdate.gameWillTerminate()`, `playdate.deviceWillSleep()`, and `playdate.deviceWillLock()`.
+function storage.save(achData)
+	local savedData = {}
+
+	savedData["$schema"] = SCHEMA_URL
+	savedData.achievements = {}
+	for _, ach in pairs(achData) do
+		table.insert(savedData.achievements, ach)
+	end
+
+	json.encodeToFile(PRIVATE_ACHIEVEMENTS_PATH, savedData)
+end
+
+return storage
