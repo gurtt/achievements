b447e1f4df7d01864de3000fb1e84c3d6a90ad3b
gurtt <6712553+gurtt@users.noreply.github.com>
2024-05-27 14:56:13 +1000

    Add files updated unit test

--- test/save.lua
+++ test/save.lua
@@ -13,7 +13,7 @@ describe("save", function()
 		_G.achievements = nil
 	end)
 
-	it("saves achievements when called", function()
+	it("saves achievements", function()
 		local booleanAch = {
 			id = rstring(),
 			name = rstring(),
@@ -51,6 +51,68 @@ describe("save", function()
 			end
 			return false
 		end
+
+		assert.is.True(contains(localSavedAchData, booleanAch))
+		assert.is.True(contains(localSavedAchData, numericAch))
+		assert.is.True(contains(sharedSavedAchData, booleanAch))
+		assert.is.True(contains(sharedSavedAchData, numericAch))
+	end)
+
+	it("overrides old saved data", function()
+		-- TODO: Remove the duplication between this and the previous test
+		local booleanAch = {
+			id = rstring(),
+			name = rstring(),
+			lockedDescription = rstring(),
+			unlockedDescription = rstring(),
+		}
+		local numericAch = {
+			id = rstring(),
+			name = rstring(),
+			lockedDescription = rstring(),
+			unlockedDescription = rstring(),
+			maxValue = 10,
+		}
+		local achDefs = defs.generate({ booleanAch, numericAch })
+		achievements.init(achDefs)
+
+		achievements.set(booleanAch.id, true)
+		achievements.set(numericAch.id, 5)
+		achievements.save()
+
+		booleanAch.value = true
+		numericAch.value = 5
+
+		local localSavedAchData = json.decodeFile("achievements.json").achievements
+		local sharedSavedAchData =
+			json.decodeFile("/Shared/Data/" .. playdate.metadata.bundleID .. "/achievements.json").achievements
+
+		local function contains(table, subject)
+			-- using ipairs because saved data shouldn't be keyed anymore
+			for _, candidate in ipairs(table) do
+				-- this isn't a full deep compare but we don't need it
+				if candidate.id == subject.id and candidate.value == subject.value then
+					return true
+				end
+			end
+			return false
+		end
+
+		assert.is.True(contains(localSavedAchData, booleanAch))
+		assert.is.True(contains(localSavedAchData, numericAch))
+		assert.is.True(contains(sharedSavedAchData, booleanAch))
+		assert.is.True(contains(sharedSavedAchData, numericAch))
+
+		achievements.set(booleanAch.id, false)
+		achievements.set(numericAch.id, 7)
+		achievements.save()
+
+		booleanAch.value = false
+		numericAch.value = 7
+
+		localSavedAchData = json.decodeFile("achievements.json").achievements
+		sharedSavedAchData =
+			json.decodeFile("/Shared/Data/" .. playdate.metadata.bundleID .. "/achievements.json").achievements
 
 		assert.is.True(contains(localSavedAchData, booleanAch))
 		assert.is.True(contains(localSavedAchData, numericAch))
